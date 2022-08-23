c3bfd717f6e2c027b5444c8d22ce2d2bab1199ba
Andrew Lundy <andrewlundy@icloud.com>
2022-08-22 21:21:24 -0500

    Finish Modules and Packages

---
+++ modules_and_packages.py
@@ -0,0 +1,12 @@
+# Exercise: https://www.learnpython.org/en/Modules_and_Packages
+# In this exercise, print an alphabetically sorted list of all the functions in the re module containing the word find.
+
+import re
+
+# Your code goes here
+find_members = []
+for member in dir(re):
+    if "find" in member:
+        find_members.append(member)
+
+print(sorted(find_members))
